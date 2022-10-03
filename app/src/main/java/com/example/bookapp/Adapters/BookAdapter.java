099da18d750e19fa8984f8335738af34f8fabb82
NahinAhmed28 <Nahin.ahmed28@gmail.com>
2022-10-03 16:52:35 +0600

    wip

---
+++ app/src/main/java/com/example/bookapp/Adapters/BookAdapter.java
@@ -0,0 +1,4 @@
+package com.example.bookapp.Adapters;
+
+public class BookAdapter {
+}
---
+++ app/src/main/java/com/example/bookapp/Models/BookModel.java
@@ -0,0 +1,11 @@
+package com.example.bookapp.Models;
+
+public class BookModel {
+    int image;
+    String bookName;
+
+    public BookModel(int image, String bookName) {
+        this.image = image;
+        this.bookName = bookName;
+    }
+}
